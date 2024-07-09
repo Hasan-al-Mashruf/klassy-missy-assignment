f81ad440264d96d38cc1aa9be612fd86397a09fb
Moeenuddin Ahmad <mashruf125@gmail.com>
2024-07-09 13:32:27 +0600

    asset image/static data updated

---
+++ staticData/data.ts
@@ -0,0 +1,28 @@
+export const selectConcernSData = [
+  { value: "Skin Care", label: "Skin Care" },
+  { value: "Hair Care", label: "Hair Care" },
+  { value: "Beauty Care", label: "Beauty Care" },
+];
+
+export const concernSData = [
+  { value: "acneBreakouts", label: "Acne & breakouts" },
+  { value: "blackheads", label: "Blackheads" },
+  { value: "drySkin", label: "Dry skin" },
+  {
+    value: "fineLinesWrinkles",
+    label: "Fine lines & wrinkles",
+  },
+  { value: "dehydratedSkin", label: "Dehydrated skin" },
+  { value: "dullSkin", label: "Dull skin" },
+  { value: "redness", label: "Redness" },
+  { value: "excessOil", label: "Excess oil" },
+  { value: "unevenTexture", label: "Uneven skin texture" },
+  { value: "acneScars", label: "Acne scars" },
+  { value: "acneSpot", label: "Acne spot" },
+  { value: "hyperpigmentation", label: "Hyperpigmentation" },
+];
+
+export const genderSData = [
+  { value: "female", label: "female" },
+  { value: "male", label: "male" },
+];
